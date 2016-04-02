05298ed57f1ed6a658b2e756dc3ed64b9817ee8f
Styp <mstypinski@gmail.com>
2016-04-02 15:16:04 +0200

    added first testmessage

---
+++ src/main/java/ch/helin/messages/MySimpleMessage.java
@@ -0,0 +1,17 @@
+package ch.helin.messages;
+
+/**
+ * Created by Martin Stypinski on 02.04.16.
+ */
+public class MySimpleMessage {
+    private String raw;
+
+    public String getRaw() {
+        return raw;
+    }
+
+    public void setRaw(String raw) {
+        this.raw = raw;
+    }
+
+}
