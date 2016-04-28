e71cbdbe1deedcb7cb7d3fe69096d76a9a9569c9
Marcel Amsler <marcel.amsler@gmail.com>
2016-04-28 13:09:55 +0200

    worked on queue names

---
+++ src/main/java/ch/helin/messages/commons/ConnectionUtils.java
@@ -0,0 +1,20 @@
+package ch.helin.messages.commons;
+
+public class ConnectionUtils {
+
+    public static String getServerSideConsumerQueueName (String token) {
+        return token + "-drone-to-server";
+    }
+
+    public static String getServerSideProducerQueueName (String token) {
+        return token + "-server-to-drone";
+    }
+
+    public static String getDroneSideConsumerQueueName (String token) {
+        return getServerSideProducerQueueName(token);
+    }
+
+    public static String getDroneSideProducerQueueName (String token) {
+        return getServerSideConsumerQueueName(token);
+    }
+}
