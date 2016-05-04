527629ef8197b91406bca963e5bebc259aa94a4c
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-04 18:06:35 +0200

    tried to fix build

--- src/main/java/ch/helin/messages/dto/message/DroneInfoMessage.java
+++ src/main/java/ch/helin/messages/dto/message/DroneInfoMessage.java
@@ -58,4 +58,30 @@ public class DroneInfoMessage extends Message{
     public void setClientTime(Date clientTime) {
         this.clientTime = clientTime;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DroneInfoMessage)) return false;
+
+        DroneInfoMessage that = (DroneInfoMessage) o;
+
+        if (batteryState != null ? !batteryState.equals(that.batteryState) : that.batteryState != null) return false;
+        if (gpsState != null ? !gpsState.equals(that.gpsState) : that.gpsState != null) return false;
+        if (droneState != null ? !droneState.equals(that.droneState) : that.droneState != null) return false;
+        if (phonePosition != null ? !phonePosition.equals(that.phonePosition) : that.phonePosition != null)
+            return false;
+        return clientTime != null ? clientTime.equals(that.clientTime) : that.clientTime == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = batteryState != null ? batteryState.hashCode() : 0;
+        result = 31 * result + (gpsState != null ? gpsState.hashCode() : 0);
+        result = 31 * result + (droneState != null ? droneState.hashCode() : 0);
+        result = 31 * result + (phonePosition != null ? phonePosition.hashCode() : 0);
+        result = 31 * result + (clientTime != null ? clientTime.hashCode() : 0);
+        return result;
+    }
 }
