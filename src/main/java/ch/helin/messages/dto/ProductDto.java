791a00288c641141c38aa5315c9a1841d49820ca
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-10 11:14:10 +0200

    worked on orderdispatching tests

--- src/main/java/ch/helin/messages/dto/ProductDto.java
+++ src/main/java/ch/helin/messages/dto/ProductDto.java
@@ -7,4 +7,24 @@ public class ProductDto {
     private Double price;
 
     private Integer weightGramm;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getWeightGramm() {
+        return weightGramm;
+    }
 }
