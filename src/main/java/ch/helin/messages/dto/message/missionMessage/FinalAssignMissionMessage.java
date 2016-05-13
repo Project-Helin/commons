3679b683ef506ef34e68afefeeafe144ab94bf0c
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-13 15:34:46 +0200

    added missionctrl test and improved testhelper

--- src/main/java/ch/helin/messages/dto/message/missionMessage/FinalAssignMissionMessage.java
+++ src/main/java/ch/helin/messages/dto/message/missionMessage/FinalAssignMissionMessage.java
@@ -10,19 +10,34 @@ import ch.helin.messages.dto.message.PayloadType;
  * the server hasn't reasigned the mission to an other drone in the meantime.
  */
 public class FinalAssignMissionMessage extends Message {
-
     private MissionDto mission;
 
     public FinalAssignMissionMessage() {
         super(PayloadType.FinalAssignMission);
     }
 
-
     public MissionDto getMission() {
         return mission;
     }
 
+
     public void setMission(MissionDto mission) {
         this.mission = mission;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof FinalAssignMissionMessage)) return false;
+
+        FinalAssignMissionMessage that = (FinalAssignMissionMessage) o;
+
+        return mission != null ? mission.equals(that.mission) : that.mission == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return mission != null ? mission.hashCode() : 0;
     }
 }
