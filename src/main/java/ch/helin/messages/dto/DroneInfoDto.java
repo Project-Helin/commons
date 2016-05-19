0d8f22d6efa6099b3f74c2b7daadf01faebeddfe
Marcel Amsler <marcel.amsler@gmail.com>
2016-05-19 15:00:41 +0200

    imroved handling of finished missions

--- src/main/java/ch/helin/messages/dto/DroneInfoDto.java
+++ src/main/java/ch/helin/messages/dto/DroneInfoDto.java
@@ -6,13 +6,23 @@ import ch.helin.messages.dto.state.GpsState;
 import ch.helin.messages.dto.way.Position;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class DroneInfoDto {
+    private UUID id;
     private BatteryState batteryState;
     private GpsState gpsState;
     private DroneState droneState;
     private Position phonePosition;
     private Date clientTime;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
     public BatteryState getBatteryState() {
         return batteryState;
@@ -28,32 +38,6 @@ public class DroneInfoDto {
 
     public void setGpsState(GpsState gpsState) {
         this.gpsState = gpsState;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DroneInfoDto)) return false;
-
-        DroneInfoDto that = (DroneInfoDto) o;
-
-        if (batteryState != null ? !batteryState.equals(that.batteryState) : that.batteryState != null) return false;
-        if (gpsState != null ? !gpsState.equals(that.gpsState) : that.gpsState != null) return false;
-        if (droneState != null ? !droneState.equals(that.droneState) : that.droneState != null) return false;
-        if (phonePosition != null ? !phonePosition.equals(that.phonePosition) : that.phonePosition != null)
-            return false;
-        return clientTime != null ? clientTime.equals(that.clientTime) : that.clientTime == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = batteryState != null ? batteryState.hashCode() : 0;
-        result = 31 * result + (gpsState != null ? gpsState.hashCode() : 0);
-        result = 31 * result + (droneState != null ? droneState.hashCode() : 0);
-        result = 31 * result + (phonePosition != null ? phonePosition.hashCode() : 0);
-        result = 31 * result + (clientTime != null ? clientTime.hashCode() : 0);
-        return result;
     }
 
     public DroneState getDroneState() {
@@ -81,4 +65,31 @@ public class DroneInfoDto {
         this.clientTime = clientTime;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DroneInfoDto)) return false;
+
+        DroneInfoDto that = (DroneInfoDto) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (batteryState != null ? !batteryState.equals(that.batteryState) : that.batteryState != null) return false;
+        if (gpsState != null ? !gpsState.equals(that.gpsState) : that.gpsState != null) return false;
+        if (droneState != null ? !droneState.equals(that.droneState) : that.droneState != null) return false;
+        if (phonePosition != null ? !phonePosition.equals(that.phonePosition) : that.phonePosition != null)
+            return false;
+        return clientTime != null ? clientTime.equals(that.clientTime) : that.clientTime == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (batteryState != null ? batteryState.hashCode() : 0);
+        result = 31 * result + (gpsState != null ? gpsState.hashCode() : 0);
+        result = 31 * result + (droneState != null ? droneState.hashCode() : 0);
+        result = 31 * result + (phonePosition != null ? phonePosition.hashCode() : 0);
+        result = 31 * result + (clientTime != null ? clientTime.hashCode() : 0);
+        return result;
+    }
 }
